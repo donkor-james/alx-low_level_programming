00de798fcecf460e422eeab4a383ab9f247168fa
Donkor James <jamesdonkor987@gmail.com>
2023-08-15 20:22:20 -0700

    nested loops and functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - main function
+* Return: always 0
+* on error, -1 is returned, and error is set appropriately.
+*/
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* main - main function
+* REturn: always 0
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
