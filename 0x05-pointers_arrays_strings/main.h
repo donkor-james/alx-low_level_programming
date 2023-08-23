fe8f04630c2c0c18495a22621746b8e8668d5a68
Donkor James <jamesdonkor987@gmail.com>
2023-08-23 11:05:53 -0700

    added 4 new files

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,9 @@
 #ifndef MAIN_H
 #define MATH_H
+void swap_int(int *a, int *b)
+int _putchar(char c);
 void reset_to_98(int *n);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
 #endif
