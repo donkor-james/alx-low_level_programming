9bdf94cb4f93b06564845127aebc053739aa3aa9
Donkor James <jamesdonkor987@gmail.com>
2023-08-22 17:35:51 -0700

    first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* reset_to_98 - update the value of a pointed address to 98
+* @n: parameter of the function which takes the address of the variable
+* Return: void
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
