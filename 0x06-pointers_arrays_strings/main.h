8a133556c8b79a7f2210916a7ef0d53b734404a8
Donkor James <jamesdonkor987@gmail.com>
2023-08-25 13:11:33 -0700

    first task - #0-strcat.c update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,5 +6,5 @@ void reset_to_98(int *n);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-cahr *-strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 #endif
