94649fa6b43f58cb80158b362dfe01c4c7ca8a04
Donkor James <jamesdonkor987@gmail.com>
2023-08-25 20:50:02 -0700

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*  - main function
-* Return: always 0
-*
-*
+* _strcmp - compare string values
+* @s1: input value
+* @s2: input value
+* Return: s1[i] - s2[i]
 */
 int  _strcmp(char *s1, char *s2)
 {
